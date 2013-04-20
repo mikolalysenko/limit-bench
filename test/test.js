c525d5656adcd8724a8090ce9048d0ce7d71df21
Mikola Lysenko <mikolalysenko@gmail.com>
2013-04-20 10:09:56 -0700

    improved precision of testing

--- test/test.js
+++ test/test.js
@@ -13,16 +13,21 @@ require("tap").test("limit-bench", function(t) {
   t.assert(counter >= 100)
 
   var start = Date.now()
-  bench(
+  counter = 0
+  var ms = bench(
     1000,
     0,
     function() {
+      var t = Date.now() + 10
+      while(Date.now() < t) {
+      }
       ++counter
     }
   )
-  t.assert(counter > 100)
   var end = Date.now()
   
   t.assert(Math.abs(end - start - 1000) < 30)
+  t.assert(Math.abs(ms - 10) < 1)
+  t.assert(Math.abs(counter - 100) < 5)
   t.end()
 })
